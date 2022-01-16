3b7c870fd17de0092bd07d5463170b3694f67d0b
Sotiris Niarchos <sot.niarchos@gmail.com>
2022-01-16 04:27:49 +0200

    added test

--- tests/bf93_testcases.rs
+++ tests/bf93_testcases.rs
@@ -1,5 +1,6 @@
 use assert_cmd::Command;
-use std::fs;
+use predicates::prelude::*;
+use std::{fs, str};
 
 pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
@@ -14,8 +15,10 @@ fn testcase(name: &str) -> String {
 #[test]
 fn test_sanity() -> Result<()> {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
-    cmd.arg(testcase("sanity"));
-    cmd.assert().success().stdout("0 1 2 3 4 5 6 7 8 9 ");
+    cmd.arg(testcase("sanity"))
+        .assert()
+        .success()
+        .stdout("0 1 2 3 4 5 6 7 8 9 ");
 
     Ok(())
 }
@@ -23,8 +26,10 @@ fn test_sanity() -> Result<()> {
 #[test]
 fn test_hello_world() -> Result<()> {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
-    cmd.arg(testcase("hello_world"));
-    cmd.assert().success().stdout("Hello world!");
+    cmd.arg(testcase("hello_world"))
+        .assert()
+        .success()
+        .stdout("Hello world!");
 
     Ok(())
 }
@@ -34,8 +39,10 @@ fn test_factorial() -> Result<()> {
     let testcase_file = testcase("factorial");
     for (query, answer) in vec![("4", "24 "), ("6", "720 "), ("10", "3628800 ")] {
         let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
-        cmd.arg(&testcase_file).write_stdin(query);
-        cmd.assert().stdout(answer);
+        cmd.arg(&testcase_file)
+            .write_stdin(query)
+            .assert()
+            .stdout(answer);
     }
 
     Ok(())
@@ -46,8 +53,20 @@ fn test_quine() -> Result<()> {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
     let filename = testcase("quine");
     let contents = fs::read_to_string(&filename)?;
-    cmd.arg(filename);
-    cmd.assert().success().stdout(contents);
+    cmd.arg(filename).assert().success().stdout(contents);
+
+    Ok(())
+}
+
+#[test]
+fn test_mycorand() -> Result<()> {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
+    let filename = testcase("mycorand");
+    let predicate_fn = predicate::str::is_match(
+        "The directions were generated in the order [<>^v]{4}\n\\? was met [0-9]+ times\n",
+    )
+    .unwrap();
+    cmd.arg(filename).assert().success().stdout(predicate_fn);
 
     Ok(())
 }
